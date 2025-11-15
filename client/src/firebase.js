e951476008afd6ec1d658b162c7d0f03a9636a6c
makej7403-bit <makej7403@gmail.com>
2025-11-15 14:44:39 +0000

    Create firebase.js

---
+++ client/src/firebase.js
@@ -0,0 +1,25 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC7cAN-mrE2PvmlQ11zLKAdHBhN7nUFjHw",
+  authDomain: "fir-u-c-students-web.firebaseapp.com",
+  databaseURL: "https://fir-u-c-students-web-default-rtdb.firebaseio.com",
+  projectId: "fir-u-c-students-web",
+  storageBucket: "fir-u-c-students-web.firebasestorage.app",
+  messagingSenderId: "113569186739",
+  appId: "1:113569186739:web:d8daf21059f43a79e841c6"
+}
+
+const app = initializeApp(firebaseConfig)
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
+
+export async function signInWithGoogle() {
+  const result = await signInWithPopup(auth, googleProvider)
+  return result.user
+}
+
+export async function signOutUser() {
+  await signOut(auth)
+}
