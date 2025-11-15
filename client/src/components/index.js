5d1a5c4596c4768937e2fa9370d77fcabb6ad747
makej7403-bit <makej7403@gmail.com>
2025-11-15 19:28:54 +0000

    Create index.js

---
+++ client/src/components/index.js
@@ -0,0 +1,4 @@
+export { default as ImageUploader } from "./ImageUploader";
+export { default as DocumentUploader } from "./DocumentUploader";
+export { default as VoiceRecorder } from "./VoiceRecorder";
+export { default as LiveCall } from "./LiveCall";
