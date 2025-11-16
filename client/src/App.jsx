0d558d942b6fe517959d0b84515461eedbea3f33
makej7403-bit <makej7403@gmail.com>
2025-11-16 00:23:49 +0000

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,10 @@
-// client/src/App.jsx
-import Sidebar from "./components/Sidebar";
-import ChatPanel from "./components/ChatPanel";
+import React from "react";
+import HomeScreen from "./components/HomeScreen";
 
 export default function App() {
   return (
-    <div className="flex h-screen w-screen overflow-hidden bg-[#f7f7f8]">
-      {/* Sidebar */}
-      <Sidebar />
-
-      {/* Chat Panel */}
-      <div className="flex flex-col flex-1 h-full">
-        <ChatPanel />
-      </div>
+    <div className="app-root">
+      <HomeScreen />
     </div>
   );
 }
